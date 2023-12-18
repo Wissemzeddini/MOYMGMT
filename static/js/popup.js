82cbc71a2f2d86906d75ad206d9e9151c4853567
ZM-Devops-T27 <devops-t27@rimans.ca>
2023-12-18 12:06:36 -0500

    create popup

--- static/js/popup.js
+++ static/js/popup.js
@@ -1,19 +1,36 @@
-document.getElementById('modal').style.display = 'block'
-window.addEventListener('scroll', function(e) {
-    setTimeout( () => {
-      document.getElementById('modal').style.display = 'block'
-    }, 2000 )
-  });
-  let modalAlreadyShowed = false
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
 
-  window.addEventListener('scroll', function(e) {
-    if( ! modalAlreadyShowed ) {
-      setTimeout( () => {
-        document.getElementById('modal').style.display = 'block'
-      }, 2000 )
-      modalAlreadyShowed = true
-    }
-  });
-  document.getElementById('modal-close').addEventListener('click', function(e) {
-    document.getElementById('modal').style.display = 'none'
+openModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget)
+    openModal(modal)
   })
+})
+
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.active')
+  modals.forEach(modal => {
+    closeModal(modal)
+  })
+})
+
+closeModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal')
+    closeModal(modal)
+  })
+})
+
+function openModal(modal) {
+  if (modal == null) return
+  modal.classList.add('active')
+  overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+  if (modal == null) return
+  modal.classList.remove('active')
+  overlay.classList.remove('active')
+}
